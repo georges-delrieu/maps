bcdae8ba2e129703f4662794b26b9d910642fa80
Riktaaa <tarik.ziari1@gmail.com>
2022-03-30 10:56:13 +0200

    small update2

--- 00_Special_Zones-001.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="00_Special_Zones-001" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="src/assets/00_Special_Zones-001.png" width="258" height="66"/>
-</tileset>
